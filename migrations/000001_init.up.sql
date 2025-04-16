6c5caa880f5c4d482a420a709b1f66774a38e5e8
Vitalya <vitamin11182005@gmail.com>
2025-04-16 13:29:04 +0300

    add users table

--- migrations/000001_init.up.sql
+++ migrations/000001_init.up.sql
@@ -2,10 +2,14 @@ CREATE TABLE IF NOT EXISTS refreshSessions (
     id SERIAL PRIMARY KEY,
     userId UUID REFERENCES users(id) ON DELETE CASCADE,
     refreshToken VARCHAR(200) NOT NULL,
-    ip VARCHAR(15) NOT NULL,
     expiresAt TIMESTAMP WITH TIME ZONE NOT NULL,
     createdAt TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
 );
+
+CREATE TABLE IF NOT EXISTS users (
+    id UUID PRIMARY KEY,
+    ip VARCHAR(15)
+)
 
 CREATE INDEX idx_refresh_tokens_userId ON refresh_tokens(userId);
 CREATE INDEX idx_refresh_tokens_token ON refresh_tokens(refreshToken);
