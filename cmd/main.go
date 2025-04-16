663390756c7b75455f8bda1251f171d792242890
Vitalya <vitamin11182005@gmail.com>
2025-04-16 13:13:26 +0300

    add app

---
+++ cmd/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "medods-test-task/internal/app"
+
+// @title Medods
+// @version 1.0
+// @description This is the test service for providing JWT
+// @host localhost
+// @BasePath /
+// @schemes https
+// @securityDefinitions.apikey BearerAuth
+// @in header
+// @name Authorization
+// @schemes http https
+
+func main() {
+	app.Run()
+}
